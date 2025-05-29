e29ece59f45318ff9b1cb74efc2a4461340289fa
Gustavo Diniz <gustavodinniz.dev@gmail.com>
2025-05-29 12:08:17 -0300

    adding internal credentials to access loan-application-service

--- loan-decision-engine/src/main/java/bank/pf/config/RestClientConfig.java
+++ loan-decision-engine/src/main/java/bank/pf/config/RestClientConfig.java
@@ -5,16 +5,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.util.Base64;
+
 @Configuration
 public class RestClientConfig {
 
     @Value("${app.loan-application-service.url}")
     private String loanApplicationServiceBaseUrl;
 
+    @Value("${app.loan-application-service.username}")
+    private String serviceUsername;
+
+    @Value("${app.loan-application-service.password}")
+    private String servicePassword;
+
     @Bean
     public RestClient loanApplicationRestClient() {
+        String authCredentials = serviceUsername + ":" + servicePassword;
+        String encodedAuthCredentials = Base64.getEncoder().encodeToString(authCredentials.getBytes());
+
         return RestClient.builder()
                 .baseUrl(loanApplicationServiceBaseUrl)
+                .defaultHeader("Authorization", "Basic " + encodedAuthCredentials)
                 .build();
     }
 }
